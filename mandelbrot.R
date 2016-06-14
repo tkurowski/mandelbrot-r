02b57814fba8f34e0a090f20c8115a9cb898d02e
tkurowski <tommy.and.friends@gmail.com>
2016-06-14 21:43:17 +0200

    use built-in complex type

--- mandelbrot.R
+++ mandelbrot.R
@@ -9,28 +9,16 @@ mandelbrot <- function (xrange = seq(-2.2, .8, length.out = 300),
                         yrange = seq(-1.5, 1.5, length.out = 300),
                         iter = 25) {
     # mesh
-    mx <- matrix(xrange, nrow = length(yrange), ncol = length(xrange))
-    my <- matrix(yrange, nrow = length(yrange), ncol = length(xrange), byrow = T)
-    
-    m <- matrix(1, nrow = length(yrange), ncol = length(xrange))
-    re <- matrix(0, nrow = length(yrange), ncol = length(xrange))
-    im <- matrix(0, nrow = length(yrange), ncol = length(xrange))
-
+    C <- complex(real=rep(xrange, times = length(yrange)),
+                 imag=rep(yrange, each = length(xrange)))
+    C <- matrix(C, 300, 300)
+    m <- matrix(1, 300, 300)
+    Z <- 0
     for (i in 1:iter) {
-        # z <- z^2 + c
-        re2 <- re^2
-        im2 <- im^2
-
-        # if mod(z) > 2 then m[_] = i
-        m[re2 + im2 > 4 & m == 1] = -i
-
-        im <- re * im * 2 + my
-        re <- re2 - im2 + mx
+        Z = Z^2 + C
+        m[Mod(Z) > 2 & m == 1] <- -i
     }
 
     m[m == 1] <- -iter
     -m
 }
-
-#image(m, col=c(heat.colors(50), "black"), asp=1)
-    
